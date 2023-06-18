efda60ee3822e0d0b62ae8f96058475b891bdc02
ognjenJ97 <ognjen.janjic97@gmail.com>
2023-06-18 21:19:52 +0200

    sharing code in a folders

---
+++ TypeScripteExercise/src/decorators/autobind.ts
@@ -0,0 +1,15 @@
+
+namespace App {
+    //autobind decoration 
+    export function autobind(_: any, _2: string, description: PropertyDescriptor) {
+    const originalMethod = description.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this)
+            return boundFn;
+        }
+    }
+    return adjDescriptor;
+}
+}
